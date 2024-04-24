278917d919e0cf774e8c9a973a801b2d66b282cb
Thais Simoes <thay.ssim@gmail.com>
2024-04-23 22:46:39 -0300

    POST orders terminado

--- internal/pkg/service/routes.go
+++ internal/pkg/service/routes.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"FullCycleCleanArchitecture/internal/pkg/repository"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"log"
@@ -19,7 +20,39 @@ func rotas(s *gin.Engine) {
 	s.POST("/order", setOrder)
 }
 
-func setOrder(c *gin.Context) {}
+func setOrder(c *gin.Context) {
+	var orders []Order
+
+	postOrders := c.PostForm("orders")
+
+	db, err := repository.OpenConnection("localhost", "postgres", "password", "postgres", 5432)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": err,
+		})
+	}
+
+	defer repository.CloseConnection(db)
+
+	err = json.Unmarshal([]byte(postOrders), &orders)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": err,
+		})
+	}
+
+	for _, order := range orders {
+		db.MustExec(`Insert into orders ("products", "total") values ($1, $2)`, order.Products, order.Total)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err,
+			})
+		}
+	}
+
+	c.IndentedJSON(http.StatusOK, "Orders inserted in database")
+
+}
 
 func getAllOrders(c *gin.Context) {
 	var order Order
